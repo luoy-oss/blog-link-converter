9178f338345f4ddee11e8c3711fcc600ed3c9ad7
drluo <2557657882@qq.com>
2025-04-01 22:12:36 +0800

    docs: 更新首页说明文字

--- index.js
+++ index.js
@@ -21,8 +21,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send(`
     <h1>博客友链格式转换器</h1>
-    <p>使用POST请求发送原始友链数据到 /api/convert 端点</p>
-    <p>或者使用GET请求：/api/convert?data=友链数据</p>
+    <p>当你看到这段文字时，说明服务已成功启动</p>
+    <p>请使用以下方式进行友链数据转换</p>
+    <p>1. 使用POST请求发送原始友链数据到 /api/convert 端口</p>
+    <p>2. 使用GET请求：/api/convert?data=友链数据</p>
     <p>项目作者: <a href="https://www.drluo.top" target="_blank">luoy-oss</a></p>
     <p>GitHub: <a href="https://www.github.com/luoy-oss" target="_blank">luoy-oss</a></p>
   `);
